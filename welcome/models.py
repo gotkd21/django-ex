408a47ed073e981cf6bab77134644e5741851ef6
dmfbrew <gotkd21@gmail.com>
2019-01-12 15:36:16 -0600

    adding table to models

--- welcome/models.py
+++ welcome/models.py
@@ -6,12 +6,12 @@ class PageView(models.Model):
     hostname = models.CharField(max_length=32)
     timestamp = models.DateTimeField(auto_now_add=True)
 
-#class Tracker(models.Model):
-#    timestamp = models.DateTimeField()
-#    curlattitude = models.DecimalField(max_digits=6, decimal_places=3)
-#    curlongtitude = models.DecimalField(max_digits=6, decimal_places=3)
-#    currentpressure = models.DecimalField(max_digits=6, decimal_places=2)
+class Tracker(models.Model):
+    timestamp = models.DateTimeField()
+    curlattitude = models.DecimalField(max_digits=6, decimal_places=3)
+    curlongtitude = models.DecimalField(max_digits=6, decimal_places=3)
+    currentpressure = models.DecimalField(max_digits=6, decimal_places=2)
 
-#    class Meta:
-#        db_table = 'weather_tracking'
-#        managed = True
+    class Meta:
+        db_table = 'weather_tracking'
+        managed = True
