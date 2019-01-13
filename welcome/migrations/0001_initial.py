2e0ffd13b7fe33579509d64293eb676cf65b9c4d
dmfbrew <gotkd21@gmail.com>
2019-01-12 18:28:20 -0600

    Added Tracker for model and view

--- welcome/migrations/0001_initial.py
+++ welcome/migrations/0001_initial.py
@@ -18,12 +18,12 @@ class Migration(migrations.Migration):
                 ('timestamp', models.DateTimeField(auto_now_add=True)),
                 ('anothername', models.CharField(max_length=5)),
             ],
-            name='Tracker'
+            name='Tracker',
             fields=[
-                timestamp = models.DateTimeField()
-                curlattitude = models.DecimalField(max_digits=6, decimal_places=3)
-                curlongtitude = models.DecimalField(max_digits=6, decimal_places=3)
-                currentpressure = models.DecimalField(max_digits=6, decimal_places=2)
-            ]
+                ('timestamp', models.DateTimeField()),
+                ('curlattitude', models.DecimalField(max_digits=6, decimal_places=3)),
+                ('curlongtitude', models.DecimalField(max_digits=6, decimal_places=3)),
+                ('currentpressure', models.DecimalField(max_digits=6, decimal_places=2)),
+            ],
         ),
     ]
