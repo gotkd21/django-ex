37170dff9937e0ff4d6aa31cd710c6cfaf87d650
dmfbrew <gotkd21@gmail.com>
2019-01-12 18:11:27 -0600

    adding field to existing table

--- welcome/models.py
+++ welcome/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class PageView(models.Model):
     hostname = models.CharField(max_length=32)
     timestamp = models.DateTimeField(auto_now_add=True)
+    anothername = models.CharField(max_length=5)
 
 
 #class Tracker(models.Model):
