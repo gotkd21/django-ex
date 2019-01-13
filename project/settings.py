4e73877cf7cd41bdbe8959a339e8abfe1dc8818d
dmfbrew <gotkd21@gmail.com>
2019-01-12 23:01:58 -0600

    corrected config class

--- project/settings.py
+++ project/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'weather_tracker.apps.weather_trackerConfig',
+    'weather_tracker.apps.WeatherTrackerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
