3689589689ec6b5e161454584e1d8f1372986219
dmfbrew <gotkd21@gmail.com>
2019-01-19 10:26:25 -0600

    update to pageview and index to show Weather data

--- weather_tracker/views.py
+++ weather_tracker/views.py
@@ -11,15 +11,18 @@ import time
 cur_time = int(time.time())
 cur_location = "42.19364,-87.96736"
 
+from django.http import HttpResponse
+
 # This is the new class created based on the direction on https://stackoverflow.com/questions/30259452/proper-way-to-consume-data-from-restful-api-in-django
 # there are problems with the definition of the class.
 # class WeatherPage(generic.Templateview):
-#    def get(self,request):
-#        weather_data = services.get_weatherdata(cur_location,cur_time)
-#        return render(request, 'index.html', weather_data)
+    def getw(request):
+        weather_data = services.get_weatherdata(cur_location,cur_time)
+        return render(request, 'index.html', {
+            wdata = weather_data[0]
+        })
 
 # Create your views here.
-from django.http import HttpResponse
 
 # This was from the trials I attempted with forms.  Never got it to work.
 #def get_data(request):
@@ -38,5 +41,6 @@ from django.http import HttpResponse
 #        'count': PageView.objects.count()
 #    })
 
-def index(request):
+# This is the original static page view creation
+# def index(request):
     return render(request, 'weather_tracker/index.html')
