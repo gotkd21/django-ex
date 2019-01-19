5125a42f3e391a2960c955a66eb72de73cf75124
dmfbrew <gotkd21@gmail.com>
2019-01-19 15:25:15 -0600

    update to index to present data 19

--- weather_tracker/services.py
+++ weather_tracker/services.py
@@ -36,14 +36,13 @@ def get_weatherdata(location,time_req):
     #loc_forecast = [bar_forecast['latitude'], bar_forecast['longitude']]
     #pressure_current = [bar_forecast['currently']['time'],bar_forecast['currently']['pressure']]
 
-    #hourly_pressure = {}
-    #hourly_pressure = [bar_forecast['hourly']['data']]
-    #print(type(hourly_pressure), type(hourly_pressure[0]))
+    hourly_pressure = [bar_forecast['hourly']['data']]
+    print(type(hourly_pressure), type(hourly_pressure[0]))
 
-    #print(hourly_pressure)
+    print(hourly_pressure)
 
 
 
     #for fcast in bar_forecast['hourly']['data']:
     #    print(fcast['time'], " ", fcast['pressure'])
-    return bar_forecast['hourly']['data']
+    return hourly_pressure
