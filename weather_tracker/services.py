a11906d42fcf11debac523d07a99bbdc001d49dd
dmfbrew <gotkd21@gmail.com>
2019-01-20 20:15:40 -0600

    cleaned up Services data response

--- weather_tracker/services.py
+++ weather_tracker/services.py
@@ -3,7 +3,10 @@
 import requests
 import time
 
+
 def get_weatherdata(location,time_req):
+
+    darkskyattributes = {'time':0,'summary':"",'precipIntensity':0,'precipProbability':0,'precipAccumulation':0,'precipType':"",'temperature':999,'apparentTemperature':999,'dewPoint':100,'humidity':999,'pressure':9999,'uvIndex':999,'visibility':999,'ozone':""}
 
     # Define key Dictionaries
     bar_forecast = {}
@@ -32,17 +35,39 @@ def get_weatherdata(location,time_req):
     cur_forecast = requests.get(WEATHERSITE + API_CALL + API_KEY + "/" + active_location_time + FLAGS)
     bar_forecast = cur_forecast.json()
 
+    print(bar_forecast)
+    forecast_loc = {}
+    forecast_loc = {'latitude': bar_forecast['latitude'], 'longitude': bar_forecast['longitude']}
+    print(forecast_loc)
     # Pull location data out of Forecast object
     #loc_forecast = [bar_forecast['latitude'], bar_forecast['longitude']]
     #pressure_current = [bar_forecast['currently']['time'],bar_forecast['currently']['pressure']]
+    hourly_data = []
 
-    hourly_pressure = [bar_forecast['hourly']['data']]
-    print(type(hourly_pressure), type(hourly_pressure[0]))
+    #print(type(bar_forecast['hourly']['data']))
+    #print(bar_forecast['hourly']['data'])
+    for fcast in bar_forecast['hourly']['data']:
+        #fcast = bar_forecast['hourly']['data'][0]
+        # print(fcast)
+        hourly_pressure = {}
+        for x,y in darkskyattributes.items():
+            if x not in fcast:
+                hourly_pressure[x]=darkskyattributes[x]
+            else:
+                hourly_pressure[x]=fcast[x]
+        hourly_data.append(hourly_pressure)
+    print(hourly_data)
 
-    print(hourly_pressure)
+
+#        print(fcast['time'], " ", fcast['pressure'])
+
+    # hourly_pressure = [bar_forecast['hourly']['data']]
+#    print(type(hourly_pressure), type(hourly_pressure[0]))
+
+    #print(hourly_data)
 
 
 
     #for fcast in bar_forecast['hourly']['data']:
     #    print(fcast['time'], " ", fcast['pressure'])
-    return hourly_pressure
+    return hourly_data
